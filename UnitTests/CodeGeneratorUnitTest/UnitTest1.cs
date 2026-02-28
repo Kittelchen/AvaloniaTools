43544cc2a76df6422f0e0415ab8c7b5f58df3a08
Kittelchen <github@kittelchen.de>
2026-02-28 11:13:37 +0100

    updated nunit template

--- UnitTests/CodeGeneratorUnitTest/UnitTest1.cs
+++ UnitTests/CodeGeneratorUnitTest/UnitTest1.cs
@@ -5,11 +5,18 @@ public class Tests
     [SetUp]
     public void Setup()
     {
+        
     }
 
     [Test]
-    public void Test1()
+    public void DummyTest01()
     {
         Assert.Pass();
+    }
+    
+    [Test]
+    public void DummyTest02()
+    {
+        Assert.Fail(message: "DummyTest02 failed!!!");
     }
 }
