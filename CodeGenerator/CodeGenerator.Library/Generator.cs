09165011e41917032f9ec96136dacb91d72330f7
Kittelchen <github@kittelchen.de>
2026-02-28 14:05:23 +0100

    version number logging

--- CodeGenerator/CodeGenerator.Library/Generator.cs
+++ CodeGenerator/CodeGenerator.Library/Generator.cs
@@ -1,12 +1,24 @@
-﻿namespace CodeGenerator.Library;
+﻿using System.Reflection;
+
+namespace CodeGenerator.Library;
 
 public class Generator
 {
     private Logger _logger;
     public bool Initialize(AppTypes type = AppTypes.CLI)
     {
+        // load config file
         var config = AppConfig.Load("config.json");
+                
         _logger = new (true, config.LogDirectory);
+        
+        // load version number
+        string dllVersion = Assembly.GetExecutingAssembly().GetName().Version?.ToString() ?? "unknown";
+        string exeVersion = Assembly.GetEntryAssembly()?.GetName().Version?.ToString() ?? "unknown";
+        
+        _logger.Debug($"Library (DLL) version: {dllVersion}");
+        _logger.Debug($"Executable (EXE) version: {exeVersion}");
+
         _logger.Success($"Config file successfully loaded!");
 
         return true;
